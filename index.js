d65ad19c50ef3515f89240deca9d1d52c93063dc
Roee Shapira <50135508+roee-shapira@users.noreply.github.com>
2020-04-28 11:22:28 +0300

    Minor edit.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const str = process.argv.slice(2)[0];
+const args = process.argv.slice(2);
+const [str] = args;
 const escStr = str.replace(/[^\0-~]/g, (c) => '\\u' + ('000' + c.charCodeAt().toString(16)).slice(-4));
 
 console.log(escStr);
